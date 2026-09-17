bea234d3eb139a2cbf5d2fe1b817695f788f1915
muge-yilmaz <mugeylmz1994@gmail.com>
2026-09-17 15:35:44 +0300

    feat: add next-themes theme provider integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,11 +23,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <UserProvider> {/* 4. Bütün uygulamayı Auth0 sarmalına aldık */}
-      <body className="min-h-full flex flex-col">{children}</body>
-      </UserProvider>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
